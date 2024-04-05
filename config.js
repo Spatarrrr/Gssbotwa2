6d4f6262b203a57af47c0ae555bc40ecd075be5c
Spatarrrr <149272739+Spatarrrr@users.noreply.github.com>
2024-04-06 00:17:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,25 +7,25 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "hunter";
+global.owner = ['254722695736'];
+global.premium = ['254722695736'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
-global.autoswview = true
+global.autoswview = false
 global.autoread = false
 global.available = false
-global.autoTyping = false
+global.autoTyping = true 
 global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false
 
 /*REPLY MESSAGE*/
 global.mess = {
